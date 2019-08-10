d0c8ac9541ce030a0e39e2ba8dae709215d2eeec
Xandronus <xandronus@gmail.com>
2019-08-10 15:53:52 -0600

    fix message updating

--- src/game.ts
+++ src/game.ts
@@ -15,20 +15,35 @@ function fetchAvatar(user, notes: Array<string>) {
         // TODO: Get avatar name from somewhere
         user.nick = 'guest'
         user.address = await getUserAccount()
-        notes.push('* Welcome ' + user.nick + ' ( ' + user.address + ' )');
-        updateTextShape(notes)
         log(user.address)
       } catch (error) {
         log(error.toString())
       }
+      try {
+        // Register user with board
+        let callUrl:string = 'https://irc-services.xandronus.now.sh/?command=getuser&nick=' + user.nick + '&create=1' + '&address=' + user.address
+        log ('getting user')
+        let response = await fetch(callUrl,{
+          headers: {
+            "Content-Type": "application/json",
+            "api_key":"ed62a076-6e98-40f4-941c-fbabc3ccfcb2"
+          }
+        })
+        let json = await response.json()
+        log('json=', json)      
+        user.id = json.message[0]._id
+      } catch (error) {
+        log(error.toString())
+      }
+      notes.push('* Welcome ' + user.nick + ' ( ' + user.address + ' )');
+      updateTextShape(notes)
     })  
 }
 
-function fetchNotes(notes: Array<string>) {
-  let callUrl:string = 'https://irc-services.xandronus.now.sh/?command=getmessages&after=2019-06-21';
+function fetchNotes(notes: Array<string>, sinceDateTime: Date) {
   executeTask(async () => {
     try {
-      
+      let callUrl:string = 'https://irc-services.xandronus.now.sh/?command=getmessages&after=' + sinceDateTime.toISOString();           
       log('getting new notes')
       let response = await fetch(callUrl,{
           headers: {
@@ -37,7 +52,8 @@ function fetchNotes(notes: Array<string>) {
           }
         })
       let json = await response.json()
-      log('json=', json)
+      sinceDateTime = new Date(Date.now())
+      log('json=', json)      
       json.message.forEach(item => {
           var ident = '< ' + item.postedby.nick + ' >';
           var messageDate = new Date(item.timestamp).toLocaleString();
@@ -52,6 +68,35 @@ function fetchNotes(notes: Array<string>) {
   return null;
 }
 
+function pushNote(user, note:string, notes: Array<string>, sinceDateTime: Date) {
+  executeTask(async () => {
+    try {
+      log('userid=', user.id)
+      var body = {
+        "userid": user.id,
+        "text": note
+      }
+      log('body=', body)
+      let callUrl:string = 'https://irc-services.xandronus.now.sh/?command=createmessage';           
+      log('add note')
+      let response = await fetch(callUrl,{
+          method: 'post',
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+            "api_key":"ed62a076-6e98-40f4-941c-fbabc3ccfcb2"
+          }
+        })
+      let json = await response.json()
+      log('json=', json)
+
+      fetchNotes(notes, sinceDateTime)
+    } catch {
+      log('failed to reach URL', error)
+    }
+  })
+  return null;
+}
 
 /// --- Set up a system ---
 class UpdateBulletinBoard implements ISystem {
@@ -68,9 +113,11 @@ class UpdateBulletinBoard implements ISystem {
 // Startup
 // ------------------------------------------------
 var notes: string[] = ["----------------- DCL BBS ------------------\nClick on board to toggle note input\n-------------------------------------------------\n"];
-var user = {nick: '', address: ''}
+var user = {nick: '', address: '', id: ''}
+var lastFetch:Date = new Date('2019-06-20')
+
 fetchAvatar(user, notes)
-fetchNotes(notes);
+fetchNotes(notes, lastFetch);
 
 const planeEntity = new Entity()
 
@@ -133,17 +180,11 @@ textInput.positionY = '25px'
 textInput.isPointerBlocker = true
 
 textInput.onTextSubmit = new OnTextSubmit(x => {
-  var ident = '< ' + user.nick + ' >';
-  var messageDate = new Date().toLocaleString()
-  notes.push(ident.padEnd(15) + messageDate.padEnd(25) + x.text)
-  updateTextShape(notes)
-//  textShape.value += x.text + '\n'
-  // const text = new UIText(textInput)
-  // text.value = x.text
-  // text.width = '100%'
-  // text.height = '20px'
-  // text.vAlign = 'top'
-  // text.hAlign = 'left'
+  pushNote(user, x.text, notes, lastFetch)
+  //var ident = '< ' + user.nick + ' >';
+  //var messageDate = new Date().toLocaleString()
+  //notes.push(ident.padEnd(15) + messageDate.padEnd(25) + x.text)
+  //updateTextShape(notes)
 });
 
 canvas.visible = false
